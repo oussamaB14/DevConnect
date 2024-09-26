53e0ef7f1771f46ff86fd7d2bad6136e5694ae12
Oussama Boufari <oussamaboufari@gmail.com>
2024-09-26 17:32:38 +0100

    Adding Tailwind css and setting up the project structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import About from './pages/About';
+import "./index.css"; // or './App.css' depending on where you placed the Tailwind CSS
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          {/* Add more routes as needed */}
+        </Routes>
+      </Router>
+    </>
   );
 }
 
